948f7942dab491c4eb7a7f58e4acde525cdf6116
Ricoch <ricardo@rootstrap.com>
2019-01-16 10:59:35 -0300

    Refactor for CodeClimate smell

--- lib/active_storage_support/base64_attach.rb
+++ lib/active_storage_support/base64_attach.rb
@@ -4,17 +4,17 @@ module ActiveStorageSupport
     module_function
 
     def attachment_from_data(attachment)
-      if attachment.is_a?(Hash)
-        base64_data = attachment.delete(:data)
-
-        if base64_data.try(:is_a?, String) && base64_data =~ /^data:(.*?);(.*?),(.*)$/
-          attachment[:io] = StringIO.new(Base64.decode64(Regexp.last_match(3)))
-          attachment[:content_type] ||= Regexp.last_match(1)
-          attachment[:filename]     ||= Time.current.to_i.to_s
-        end
-      end
+      fill_attachment_data(attachment, attachment.delete(:data)) if attachment.is_a?(Hash)
 
       attachment
+    end
+
+    def fill_attachment_data(attachment, base64_data)
+      return unless base64_data.try(:is_a?, String) && base64_data =~ /^data:(.*?);(.*?),(.*)$/
+
+      attachment[:io] = StringIO.new(Base64.decode64(Regexp.last_match(3)))
+      attachment[:content_type] ||= Regexp.last_match(1)
+      attachment[:filename]     ||= Time.current.to_i.to_s
     end
   end
 end
