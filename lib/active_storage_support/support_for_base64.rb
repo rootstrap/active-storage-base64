dc52cff60af0c3d3a10f73899ed654b397bb8d97
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2020-07-18 21:48:38 -0300

    Support Rails 6.1

--- lib/active_storage_support/support_for_base64.rb
+++ lib/active_storage_support/support_for_base64.rb
@@ -5,12 +5,14 @@ module ActiveStorageSupport
   module SupportForBase64
     extend ActiveSupport::Concern
     class_methods do
-      def has_one_base64_attached(name, dependent: :purge_later)
-        has_one_attached name, dependent: dependent
+      def has_one_base64_attached(name, dependent: :purge_later, service: nil, strict_loading: false)
+        has_one_attached name, dependent: dependent, service: service, strict_loading: strict_loading
 
         generated_association_methods.class_eval <<-CODE, __FILE__, __LINE__ + 1
+          # frozen_string_literal: true
           def #{name}
-            @active_storage_attached_#{name} ||= ActiveStorageSupport::Base64One.new("#{name}", self)
+            @active_storage_attached ||= {}
+            @active_storage_attached[:#{name}] ||= ActiveStorageSupport::Base64One.new("#{name}", self)
           end
           def #{name}=(attachable)
             attachment_changes["#{name}"] =
@@ -25,12 +27,14 @@ module ActiveStorageSupport
         CODE
       end
 
-      def has_many_base64_attached(name, dependent: :purge_later)
-        has_many_attached name, dependent: dependent
+      def has_many_base64_attached(name, dependent: :purge_later, service: nil, strict_loading: false)
+        has_many_attached name, dependent: dependent, service: service, strict_loading: strict_loading
 
         generated_association_methods.class_eval <<-CODE, __FILE__, __LINE__ + 1
+          # frozen_string_literal: true
           def #{name}
-            @active_storage_attached_#{name} ||= ActiveStorageSupport::Base64Many.new("#{name}", self)
+            @active_storage_attached ||= {}
+            @active_storage_attached[:#{name}] ||= ActiveStorageSupport::Base64Many.new("#{name}", self)
           end
           def #{name}=(attachables)
             if ActiveStorage.replace_on_assign_to_many
