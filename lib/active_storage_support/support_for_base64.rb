d49dac29fce83ec10974e48e41eac875c3c963da
quangatlas <quang@atlas.kitchen>
2022-07-13 15:03:15 +0700

    fix: allow empty hash to nullify attachable

--- lib/active_storage_support/support_for_base64.rb
+++ lib/active_storage_support/support_for_base64.rb
@@ -17,7 +17,7 @@ module ActiveStorageSupport
 
           def #{name}=(attachable)
             attachment_changes["#{name}"] =
-              if attachable.nil?
+              if attachable.presence.nil?
                 ActiveStorage::Attached::Changes::DeleteOne.new("#{name}", self)
               else
                 ActiveStorage::Attached::Changes::CreateOne.new(
