8c4e20da48f06dcdf91f078160d9e3561a0c0b7c
Abdullah Baaboud <Abdullah.Ba-Aboud@jisr.net>
2024-01-08 09:22:43 +0300

    fix empty file decode issue

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many_base64_attached :pictures do |attachable|
     attachable.variant :thumb, resize_to_fit: [1, 1]
   end
+
+  has_one_base64_attached :file
 end
